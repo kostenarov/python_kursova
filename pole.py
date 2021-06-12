455b621c75eaf24c2ebd015d267fda3c40bcbfa8
Krumomir <61614440+Krumomir@users.noreply.github.com>
2021-06-12 22:57:39 +0300

    Update pole.py

--- pole.py
+++ pole.py
@@ -1,9 +1,10 @@
 import pygame
 
-
 pygame.init()
 
 screen = pygame.display.set_mode((800, 500))
+
+counter = 0
 
 
 def button(screen, position, text):
@@ -15,13 +16,20 @@ def button(screen, position, text):
     pygame.draw.line(screen, (144, 238, 144), (x, y - 2), (x, y + h), 5)
     pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
     pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
-    pygame.draw.rect(screen, (100, 150, 100), (x, y, w, h))
+    pygame.draw.rect(screen, (128, 128, 128), (x, y, w, h))
     return screen.blit(text_render, (x, y))
 
 
-def button2(screen, position, text):
+def button2(screen, position, text, color):
     font = pygame.font.SysFont("Arial", 24)
-    text_render = font.render(text, 1, (255, 0, 0))
+    if color == "blue":
+        text_render = font.render(text, 1, (0, 0, 255))
+    elif color == "green":
+        text_render = font.render(text, 1, (0, 255, 0))
+    elif color == "red":
+        text_render = font.render(text, 1, (255, 0, 0))
+    elif color == "pink":
+        text_render = font.render(text, 1, (255, 182, 193))
     x, y, w, h = text_render.get_rect()
     x, y = position
     pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
@@ -33,53 +41,59 @@ def button2(screen, position, text):
 
 
 def buildings_menu():
-    global buy_factory, buy_cleaning, buy_windturbine
-    buy_factory = button2(screen, (500, 100), "          BUY FACTORY         ")
-    buy_cleaning = button2(screen, (500, 150), "BUY CLEANING STATION")
-    buy_windturbine = button2(screen, (500, 200), "          BUY WINDMILL         ")
+    global buy_factory, buy_cleaning, buy_windturbine, exit
+    buy_factory = button2(screen, (500, 100), "          BUY FACTORY         ", "red")
+    buy_cleaning = button2(screen, (500, 150), "BUY CLEANING STATION", "blue")
+    buy_windturbine = button2(screen, (500, 200), "          BUY WINDMILL         ", "green")
+    exit = button2(screen, (500, 300), "                    EXIT                  ", "red")
+
+
+def buy_menu():
+    global buy_land
+    buy_land = button2(screen, (500, 250), "                    BUY                   ", "pink")
 
 
 def menu():
-    global buy_factory, buy_cleaning, buy_windturbine
-    b1 = button(screen, (100, 100), "      ")
-    b2 = button(screen, (200, 100), "      ")
-    b3 = button(screen, (300, 100), "      ")
-    b4 = button(screen, (100, 200), "      ")
-    b5 = button(screen, (200, 200), "      ")
-    b6 = button(screen, (300, 200), "      ")
-    b7 = button(screen, (100, 300), "      ")
-    b8 = button(screen, (200, 300), "      ")
-    b9 = button(screen, (300, 300), "      ")
+    global buy_factory, buy_cleaning, buy_windturbine, buy_land, counter, exit
+
+    b = [[11, 12, 5], [15, 6, 10], [10, 8, 12], [12, 15, 8]]
+
+    for i in range(3):
+        for j in range(3):
+            if i == 1:
+                b[i][j] = button(screen, ((100 * (j + 1) ), 100), "      ")
+            elif i == 2:
+                b[i][j] = button(screen, ((100 * (j + 1)), 200), "      ")
+            else:
+                b[i][j] = button(screen, ((100 * (j + 1)), 300), "      ")
 
     while True:
         mx, my = pygame.mouse.get_pos()
         for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
+            if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if b1.collidepoint(mx, my):
-                    buildings_menu()
-                elif b2.collidepoint(mx, my):
-                    buildings_menu()
-                elif b3.collidepoint(mx, my):
-                    buildings_menu()
-                elif b4.collidepoint(mx, my):
-                    buildings_menu()
-                elif b5.collidepoint(mx, my):
-                    buildings_menu()
-                elif b6.collidepoint(mx, my):
-                    buildings_menu()
-                elif b7.collidepoint(mx, my):
-                    buildings_menu()
-                elif b8.collidepoint(mx, my):
-                    buildings_menu()
-                elif b9.collidepoint(mx, my):
-                    buildings_menu()
+                for i in range(0, 3):
+                    for j in range(0, 3):
+                        if b[i][j].collidepoint(mx, my):
+                            buy_menu()
+                            buildings_menu()
+                            save_i = i
+                            save_j = j
+                            pass
+
                 if buy_factory.collidepoint(mx, my):
-                    print("factory")
+                    pygame.draw.rect(screen, (255, 0, 0), b[save_i][save_j])
                 if buy_cleaning.collidepoint(mx, my):
-                    print("cleaning station")
+                    pygame.draw.rect(screen, (0, 0, 255), b[save_i][save_j])
                 if buy_windturbine.collidepoint(mx, my):
-                    print("windturbine")
+                    pygame.draw.rect(screen, (0, 255, 0), b[save_i][save_j])
+                if buy_land.collidepoint(mx, my):
+                    pygame.draw.rect(screen, (255, 182, 193), b[save_i][save_j])
+
+
+
         pygame.display.update()
+
+
 menu()
